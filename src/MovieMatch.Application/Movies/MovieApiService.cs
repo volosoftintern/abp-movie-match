68337bbc64636702e50eb6c414a9a407c598c558
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-07-19 11:24:10 +0300

    add imdb api lib

---
+++ src/MovieMatch.Application/Movies/MovieApiService.cs
@@ -0,0 +1,29 @@
+﻿using IMDbApiLib;
+using IMDbApiLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace MovieMatch.Movies
+{
+    public class MovieApiService:ITransientDependency
+    {
+        private readonly ApiLib _apiLib;
+
+        public MovieApiService()
+        {
+            _apiLib=new ApiLib(MovieApiConstants.ApiKey);
+        }
+
+        public async Task<SearchData> SearchMovieAsync(string name)
+        {
+             var result= await _apiLib.SearchMovieAsync(name);
+            return result;  
+        }
+
+    }
+}
