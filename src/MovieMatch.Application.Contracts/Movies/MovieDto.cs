160cee99107b39b0ab081fec9a951ae9aea8f720
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-07-19 11:28:36 +0300

    create movie dto

---
+++ src/MovieMatch.Application.Contracts/Movies/MovieDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieMatch.Movies
+{
+    public class MovieDto
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+
+    }
+}
