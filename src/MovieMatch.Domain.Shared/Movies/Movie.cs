16f3217678fdb25d2cd1ba9b4efe0b237fcaa085
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-07-19 11:29:20 +0300

    create movie constants

---
+++ src/MovieMatch.Domain.Shared/Movies/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieMatch.Movies
+{
+    public class MovieApiConstants
+    {
+        public const string ApiKey = "k_7s5g7fm5";
+    }
+}
