52fc05e790b9eb186360570976bdcfad8b6751f1
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-07-20 11:42:38 +0300

    create movie app service for retrieving movie detail

---
+++ src/MovieMatch.Application/Movies/MovieAppService.cs
@@ -0,0 +1,25 @@
+﻿using Volo.Abp.ObjectMapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DM.MovieApi.MovieDb.Movies;
+
+namespace MovieMatch.Movies
+{
+    public class MovieAppService : MovieMatchAppService, IMovieAppService
+    {
+        private readonly MovieApiService _movieApiService;
+        public MovieAppService(MovieApiService movieApiService)
+        {
+            _movieApiService = movieApiService;
+        }
+
+        public async Task<MovieDetailDto> GetAsync(int id)
+        {
+            var result= await _movieApiService.GetMovieAsync(id);
+            return ObjectMapper.Map<Movie, MovieDetailDto>(result);
+        }
+    }
+}
