5e495c11414f9e0b8e1fb35b5a9c90c762ce670c
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-07-27 11:34:42 +0300

    create popular movie dto

---
+++ src/MovieMatch.Application.Contracts/Search/PopularMovieDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MovieMatch.Search
+{
+    public class PopularMovieDto
+    {
+        [Required]
+        public int CurrentPage { get; set; } = 1;
+    }
+}
