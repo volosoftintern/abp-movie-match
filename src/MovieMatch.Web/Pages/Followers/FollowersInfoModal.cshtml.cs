36a19c096c2f66a2fea0afd662ea7448b9eb05e1
cagatayalp-cpu23 <71559331+cagatayalp-cpu23@users.noreply.github.com>
2022-08-02 14:19:19 +0300

    create modals for followers and followings

---
+++ src/MovieMatch.Web/Pages/Followers/FollowersInfoModal.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace MovieMatch.Web.Pages.Followers
+{
+    public class FollowersInfoModalModel : AbpPageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/MovieMatch.Web/Pages/Following/FollowingInfoModal.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace MovieMatch.Web.Pages.Following
+{
+    public class FollowingInfoModalModel : AbpPageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
