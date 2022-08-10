7832539210c3e5c14ebbaef3528da52ed1a94355
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2022-08-10 15:27:39 +0300

    add post feed pagination

--- src/MovieMatch.Application.Contracts/Posts/PostFeedDto.cs
+++ src/MovieMatch.Application.Contracts/Posts/PostFeedDto.cs
@@ -5,6 +5,8 @@ namespace MovieMatch.Posts
     public class PostFeedDto
     {
         public Guid UserId { get; set; }
+        public int MaxCount { get; set; }
+        public int SkipCount { get; set; }
 
     }
 }
