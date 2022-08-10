06f61b8523c8c71021f1b8e458fb1542b0f1edeb
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2022-08-10 15:26:38 +0300

    bring user feed

--- src/MovieMatch.Web/Pages/Index.js
+++ src/MovieMatch.Web/Pages/Index.js
@@ -1,5 +1,9 @@
 $(function () {
 
+    var skipCount = 0;
+    var maxResultCount = 10;
+    var resultCount = 0;
+
     $('#searchBtn').on('click', (e) => {
         const movieName = $('#movieName').val();
         if (isNullOrEmpty(movieName)) return;
@@ -68,45 +72,197 @@ $(function () {
     getPosts = async () => {
         await $('.loader').fadeIn().promise();
 
-        await $('.post-list').fadeOut().promise();
+        //await $('.post-list').fadeOut().promise();
         
-        movieMatch.posts.post.getFeed({ userId: `${abp.currentUser.id}` }).done(async (res) => {
-            await $('.loader').fadeOut().promise;
+        movieMatch.posts.post.getFeed({ userId: `${abp.currentUser.id}`,maxCount:maxResultCount,skipCount,skipCount }).done(async (res) => {
+            await $('.loader').fadeOut().promise();
 
-            await $('.post-list').fadeIn().promise();
+            //await $('.post-list').fadeIn().promise();
+
+            renderResults(res.items);
             
-            if (res.items.length> 0) {
-                //render results
-                renderResults(res.items);
-            } else {
-                $('#post-list').append('<h2> No Post Data</h2>');
-            }
+
+            if (res.items.length < maxResultCount) {
+
+                if (res.items.length == 0) {
+                    await $('.no-post-data').fadeIn().promise();
+                } else {
+                    $('.btn-load-more').prop('disabled', true);
+                    $('.btn-load-more').text('No More Data');
+
+                }
+            } 
+
+            if (res.items.length>0)
+                await $('.btn-load-more').fadeIn().promise();
             
+            skipCount += res.items.length;
+           
         });
     }
 
     renderResults = (posts) => {
-        $('#post-list').empty();
 
         posts.forEach((val, i) => {
             $('#post-list').append(`
-                <div class="card" style="width: 18rem;">
+                <div class="card" >
                     <div class="card-body">
-                        <h5 class="card-title">${val.username}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${val.movieTitle}</h6>
-                        <p class="card-text">${val.comment}</p>
-                        <p class="card-text"><i class="fas fa-star"></i> ${val.rate}</p>
+                        <h5 class="card-title">${val.user.name}<span class="text-muted px-1">@${val.user.userName}</span><span class="px-1">&#x26AC;</span><span class="text-muted px-1">${getTimeDiffer(`${val.creationTime}`)}</span> </h5>
+                        <h6 class="card-subtitle text-muted mb-1">${val.movie.title}</h6>
+                        <a href="/Movies/${val.movie.id}">
+                            <div class="row mb-1">
+                                <div class="col-2">
+                                    <img class="card-img-top" src="https://image.tmdb.org/t/p/original/${val.movie.posterPath}" alt="${val.movie.title}">
+                                </div>
+                                <div class="col-7">
+                                    <p class="card-text movie-comment">${val.comment}</p>
+                                </div>
+                                <div class="col-2">
+                                    <p class="card-text"><i class="fas fa-star"></i> ${val.rate}</p>
+                                </div>
+                            </div>
+                        </a>
+
                     </div>
                 </div>
             `)
+
+
+            $(`#${val.movie.id}.btn-watch-later`).data('isactivewatchlater', val.movie.isActiveWatchLater)
+            $(`#${val.movie.id}.btn-watch-later`).on('click', () => {
+                const isActiveWatchLater = $(`#${val.movie.id}.btn-watch-later`).data('isactivewatchlater');
+                if (isActiveWatchLater == false) {
+                    addWatchLater(val.movie.id, abp.currentUser.id)
+                }
+                else {
+                    removeFromWatchLaterList(val.movie.id)
+                }
+
+            });
+
+            $(`#${val.movie.id}.btn-watched-before`).data('isactivewatchedbefore', val.movie.isActiveWatchedBefore)
+            $(`#${val.movie.id}.btn-watched-before`).on('click', () => {
+                const isActiveWatchedBefore = $(`#${val.movie.id}.btn-watched-before`).data('isactivewatchedbefore');
+                if (isActiveWatchedBefore == false) {
+                    addWatchedBefore(val.movie.id, abp.currentUser.id)
+                }
+                else {
+                    removeFromWatchedBeforeList(val.movie.id)
+                }
+            })
+
         })
     }
+
+    addWatchLater = (movieId, userId) => {
+        movieMatch.moviesWatchLater.watchLater.create({ userId: userId, movieId: movieId }).done((res) => {
+            $(`#${movieId}.btn-watch-later`).data('isactivewatchlater', true);
+            $(`#${movieId}.btn-watch-later`).toggleClass("btn-primary btn-secondary");
+            $(`#${movieId}.btn-watch-later`).text("Remove Watch Later");
+            abp.notify.success(
+                'Movie added watch later list.',
+                'Success'
+            );
+        });
+    }
+    addWatchedBefore = (movieId, userId) => {
+        movieMatch.moviesWatchedBefore.watchedBefore.create({ userId: userId, movieId: movieId }).done((res) => {
+            $(`#${movieId}.btn-watched-before`).data('isactivewatchedbefore', true);
+            $(`#${movieId}.btn-watched-before`).toggleClass("btn-primary btn-secondary");
+            $(`#${movieId}.btn-watched-before`).text("Remove Watched Before");
+            abp.notify.success(
+                'Movie added watched before list.',
+                'Success'
+            );
+        });
+    }
+
+    removeFromWatchLaterList = (id) => {
+        abp.message.confirm('Are you sure?')
+            .then((confirmed) => {
+                if (confirmed) {
+                    movieMatch.movies.movie
+                        .deleteMoviesWatchLater(id)
+                        .then(() => {
+                            $(`#${id}.btn-watch-later`).data('isactivewatchlater', false);
+                            abp.notify.info("Successfully deleted!");
+                            $(`#${id}.btn-watch-later`).toggleClass("btn-secondary btn-primary");
+                            $(`#${id}.btn-watch-later`).text("Add Watch Later");
+                            
+                        })
+                }
+            });
+    };
+    removeFromWatchedBeforeList = (id) => {
+        abp.message.confirm('Are you sure?')
+            .then((confirmed) => {
+                if (confirmed) {
+                    movieMatch.movies.movie
+                        .deleteMoviesWatchedBefore(id)
+                        .then(() => {
+                            $(`#${id}.btn-watched-before`).data('isactivewatchedbefore', false);
+                            abp.notify.info("Successfully deleted!");
+                            $(`#${id}.btn-watched-before`).toggleClass("btn-secondary btn-primary");
+                            $(`#${id}.btn-watched-before`).text("Add Watched Before");
+                        })
+                }
+            });
+    };
 
 
     isNullOrEmpty = (str) => {
         return str === null || str.match(/^ *$/) !== null;
     }
 
+    loadMore =async (skipCount, maxResultCount) => {
+
+        await $('.btn-load-more').fadeOut().promise();
+
+        movieMatch.posts.post.getFeed({ userId: `${abp.currentUser.id}`, maxCount: maxResultCount, skipCount, skipCount }).done(async (res) => {
+
+            await $('.loader').fadeOut().promise();
+
+            renderResults(res.items);
+           
+
+            if (res.items.length < maxResultCount) {
+                $('.btn-load-more').prop('disabled', true);
+                $('.btn-load-more').html('No More Data');
+                
+            }
+
+            skipCount += res.items.length;
+
+            await $('.btn-load-more').fadeIn().promise();
+
+        });
+    }
+
+
+    $('.btn-load-more').on('click', () => {
+        loadMore(skipCount, maxResultCount);
+    });
+
+    getTimeDiffer=(creationTime)=>{
+
+        const date = new Date(creationTime);
+        const currentTime = new Date();
+        const diff = currentTime.getTime() - date.getTime();
+        const hour =  60 *60*1000;
+        const day = 24 * hour;
+        const month = 30 * day;
+
+        if (diff < hour) {
+            return `${Math.floor(diff / 60)}m`;
+        } else if (diff < day) {
+            return `${Math.floor(diff / hour)}h`;
+        } else if (diff < month) {
+            return `${Math.floor(diff / day)}d`;
+        } 
+
+        return date.toLocaleDateString();
+       
+    }
 
     getPosts();
     
