f22ab35c577dc8990c5f268d3f1ade78a27fa2ac
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2022-08-21 01:07:25 +0300

    login page auth guard

--- src/angular/src/app/app-routing.module.ts
+++ src/angular/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from '@abp/ng.core';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -5,6 +6,7 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
+    canActivate:[AuthGuard],
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
   },
   {
