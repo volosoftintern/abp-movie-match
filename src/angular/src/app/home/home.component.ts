27c259df824510c1095e918a3a99ad96d3b57fcc
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2022-08-21 01:08:19 +0300

    add infinite scroll

--- src/angular/src/app/home/home.component.ts
+++ src/angular/src/app/home/home.component.ts
@@ -1,8 +1,7 @@
 import { AuthService, PagedResultDto, ConfigStateService, CurrentUserDto } from '@abp/ng.core';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { PostDto, PostService } from '@proxy/posts';
 import { OAuthService } from 'angular-oauth2-oidc';
-import { IdentityUserService } from '@abp/ng.identity/proxy';
 
 @Component({
   selector: 'app-home',
@@ -10,28 +9,70 @@ import { IdentityUserService } from '@abp/ng.identity/proxy';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+
   post = { items: [], totalCount: 0 } as PagedResultDto<PostDto>;
-  
+  maxCount = 5
+  skipCount = 0;
+  isLoading = false;
+  noData = false;
+  noMoreData = false;
+  currentUser: CurrentUserDto;
+  @ViewChild('postList') postList: ElementRef<HTMLDivElement>;
+
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll($event) {
+
+    if (this.noMoreData || this.isLoading) return;
+    
+    let offset = document.documentElement.scrollTop + window.innerHeight;
+    let height = document.documentElement.offsetHeight;
+
+    if (offset >= height) {
+      this.loadPosts(this.currentUser);
+    }
+
+  }
+
+
   get hasLoggedIn(): boolean {
     return this.oAuthService.hasValidAccessToken();
   }
 
   constructor(
     private oAuthService: OAuthService,
-    private authService: AuthService, private postService: PostService,
-    private identityService: IdentityUserService, private configStateService: ConfigStateService) { }
+    private authService: AuthService,
+    private postService: PostService,
+    private configStateService: ConfigStateService) { }
 
   ngOnInit(): void {
-    const currentUser = this.configStateService.getOne('currentUser') as CurrentUserDto;
-    this.postService.getFeed({ userId: currentUser.id, maxCount: 10, skipCount: 0 }).subscribe(res => this.post = res);
-
+    this.currentUser = this.configStateService.getOne('currentUser') as CurrentUserDto;
+    this.loadPosts(this.currentUser);
   }
 
   login() {
     this.authService.navigateToLogin();
   }
 
-  
+  loadPosts(currentUser: CurrentUserDto) {
+    this.isLoading = true;
+    this.postService.getFeed({ userId: currentUser.id, maxCount: this.maxCount, skipCount: this.skipCount }).subscribe(res => {
+      this.post.items.push(...res.items);
+
+      this.post.totalCount = res.totalCount
+      this.skipCount += res.items.length;
+
+      if (res.totalCount == 0) {
+        this.noData = true
+
+      } else if (res.items.length < this.maxCount) {
+
+        this.noMoreData = true;
+      }
+      this.isLoading = false;
+    });
+  }
+
 }
 
 
