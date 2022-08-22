ef9c99e98baa4826d73f0ff60bf3df3dcba24385
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2022-08-22 17:49:42 +0300

    make person movie carousel infinite scroll

--- src/MovieMatch.Web/Pages/Movies/Actor.cshtml.cs
+++ src/MovieMatch.Web/Pages/Movies/Actor.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MovieMatch.Movies;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MovieMatch.Web.Pages.Movies
@@ -10,8 +11,11 @@ namespace MovieMatch.Web.Pages.Movies
         [BindProperty(SupportsGet = true)]
         public int ActorId { get; set; }
         public PersonDto Actor { get; set; }
+        public IReadOnlyList<MovieDto> Movies { get; set; }
+        public long TotalCount { get; set; }
 
         private IMovieAppService _movieAppService;
+
 
         public ActorModel(IMovieAppService movieAppService)
         {
@@ -28,6 +32,9 @@ namespace MovieMatch.Web.Pages.Movies
         private async Task GetDataAsync()
         {
             Actor = await _movieAppService.GetPersonAsync(ActorId);
+            var response= await _movieAppService.GetPersonMoviesAsync(new PersonMovieRequestDto { PersonId = ActorId});
+            Movies = response.Items;
+            TotalCount = response.TotalCount;
         }
     }
 }
--- src/MovieMatch.Web/Pages/Movies/Actor.js
+++ src/MovieMatch.Web/Pages/Movies/Actor.js
@@ -1,15 +1,18 @@
 ﻿$(function () {
 
+    let skipCount = 0;
+    const maxCount = 20;
+    var totalCount = 0;
+
     var swiper = new Swiper(".movie-carousel", {
         slidesPerView: 5,
         spaceBetween: 10,
-        centeredSlides: true,
         allowTouchMove: true,
         allowClick:true,
-        loop: true,
+        loop: false,
         direction: 'horizontal',
         autoHeight: true,
-        centeredSlides: true,
+        centeredSlides: false,
         pagination: {
             el: '.swiper-pagination',
             clickable: true
@@ -21,9 +24,39 @@
     });
 
 
-    //swiper.on('click', (element, event) => {
-    //    const id = ($(event.target).data('id'))
-
+    swiper.on('reachEnd', () => {
+        if (swiper.slides.length < maxCount || swiper.slides.length == totalCount) return;
+        let pageNumber = parseInt(swiper.slides.length / maxCount);
         
-    //})
+        skipCount = swiper.slides.length;
+
+        movieMatch.movies.movie.getPersonMovies({
+            skipCount: skipCount,
+            personId: $('.person-card').data('personid'),
+            isDirector: false,
+            pageNumber: pageNumber+1
+        }).done(async (res) => {
+
+            totalCount = res.totalCount;
+            await renderResults(res);
+            swiper.update();
+        })
+    });
+
+    renderResults = async (res) => {
+        return new Promise((resolve, reject) => {
+            res.items.forEach((val, index) => {
+
+                $('.swiper-wrapper').append(`
+                    <div class="swiper-slide">
+                        <a href="/Movies/${val.id}">
+                            <img alt="${val.title}" src="https://image.tmdb.org/t/p/original/${val.posterPath}" />
+                        </a>
+                    </div>
+                `);
+
+            })
+            resolve();
+        })
+    }
 });
